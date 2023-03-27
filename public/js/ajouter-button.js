f5d08b8028cfa1bdfb681e185757b342b281e759
Teuletc <corentin.teulet@gmail.com>
2023-03-27 17:40:35 +0200

    njj

--- public/js/ajouter-button.js
+++ public/js/ajouter-button.js
@@ -1,10 +1,35 @@
 const newButton = document.getElementById('new-button');
+const modifButton = document.querySelectorAll('.modif-button');
 const popupContainer = document.querySelector('.popup-container');
 const closeBtn = document.querySelector('.close-btn');
+
+// Récupération des champs du formulaire
+// const titreInput = document.getElementById('titre-input');
+// const entrepriseInput = document.getElementById('entreprise-input');
+// const tagsInput = document.getElementById('tags-input');
+// const villeInput = document.getElementById('ville-input');
+// const codepostalInput = document.getElementById('codepostal-input');
+// const descriptionInput = document.getElementById('description-input');
+
 
 newButton.addEventListener('click', () => {
   popupContainer.style.display = 'flex';
 });
+
+modifButton.forEach(button => {
+  button.addEventListener('click', () => {
+    popupContainer.style.display = 'flex';
+
+    // Modification des placeholders des champs du formulaire
+    // titreInput.placeholder = "{{Nouveau placeholder pour le titre}}";
+    // entrepriseInput.placeholder = "{{$offre -> name_companies}}";
+    // tagsInput.placeholder = "Nouveau placeholder pour les tags";
+    // villeInput.placeholder = "Nouveau placeholder pour la ville";
+    // codepostalInput.placeholder = "Nouveau placeholder pour le code postal";
+    // descriptionInput.placeholder = "Nouveau placeholder pour la description";
+  });
+});
+
 
 closeBtn.addEventListener('click', () => {
   popupContainer.style.display = 'none';
--- resources/views/admin-offer.blade.php
+++ resources/views/admin-offer.blade.php
@@ -24,7 +24,7 @@
 
 </head>
 <body>
-<nav class="navbar">
+    <nav class="navbar">
         <ul>
             <li><a class="logo" href="admin-companie"><img src="{{ asset('imgs/logopng.png')}}" alt=""></a></li>
             <div class="menu-nav">
@@ -35,9 +35,7 @@
                 <li id="new-button"><a href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-11H7v2h4v4h2v-4h4v-2h-4V7h-2v4z"/></svg>AJOUTER</a></li>
                 
-                <li id="logout-button"><a href="auth-page"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 11h8v2H5v3l-5-4 5-4v3zm-1 7h2.708a8 8 0 1 0 0-12H4A9.985 9.985 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10a9.985 9.985 0 0 1-8-4z"/></svg>
-                    
-                    LOGOUT</a></li>            
+                <li id="logout-button"><a href="auth-page"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 11h8v2H5v3l-5-4 5-4v3zm-1 7h2.708a8 8 0 1 0 0-12H4A9.985 9.985 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10a9.985 9.985 0 0 1-8-4z"/></svg>LOGOUT</a></li>            
             </div>
         </ul>
     </nav>
@@ -106,8 +104,10 @@
                             <td>{{substr($offre -> created_at,0,10)}}</td>
                             <td>{{$offre -> nbr_students_places}}</td>
                             <td><div class="modif-box">
-                              <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm4 10v4h2v-4H7zm4-6v10h2V7h-2zm4 3v7h2v-7h-2z"/></svg></a>
-                              <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16.757 3l-7.466 7.466.008 4.247 4.238-.007L21 7.243V20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12.757zm3.728-.9L21.9 3.516l-9.192 9.192-1.412.003-.002-1.417L20.485 2.1z"/></svg></a>
+                              <!-- <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm4 10v4h2v-4H7zm4-6v10h2V7h-2zm4 3v7h2v-7h-2z"/></svg></a> -->
+
+                              <a class="modif-button compagnie-{{$offre -> id}}" href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16.757 3l-7.466 7.466.008 4.247 4.238-.007L21 7.243V20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12.757zm3.728-.9L21.9 3.516l-9.192 9.192-1.412.003-.002-1.417L20.485 2.1z"/></svg></a>
+
                               <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm-8 5v6h2v-6H9zm4 0v6h2v-6h-2zM9 4v2h6V4H9z"/></svg></a>   
                             </div></td>
                           </tr>
